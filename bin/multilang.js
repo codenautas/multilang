2f588230bb5fd1f040f05af400d8dccb1cb75e15
Diego F.(W) <diegoefe@gmail.com>
2015-04-08 19:06:08 -0300

    obtainLangs: corrección en la regex

--- bin/multilang.js
+++ bin/multilang.js
@@ -13,28 +13,33 @@ multilang.changeDoc=function changeDoc(documentText,lang){
 multilang.obtainLangs=function obtainLangs(documentTextHeader){
     var all_langs = [];
     var def_lang = null; 
-    var langs = /<!--multilang v[0-9]+\s+([^-]+)-->/.exec(documentTextHeader);
-    var lang_re = /([a-z]{2}):([a-zA-Z]+\.html)/g;
-    var lang;
-    while(null != (lang = lang_re.exec(langs[1]))) {
-        if(null == def_lang) { def_lang = lang[1]; }
-        all_langs[lang[1]] = {'fileName' : lang[2] };
+    var langs = /<!--multilang v[0-9]+\s+(.+)(-->)/.exec(documentTextHeader);
+    if(langs) {
+        var lang_re = /([a-z]{2}):([^.]+\.(md|html))/g;
+        var lang;
+        while(null != (lang = lang_re.exec(langs[1]))) {
+            if(null == def_lang) { def_lang = lang[1]; }
+            all_langs[lang[1]] = {'fileName' : lang[2] };
+        }
     }
     return {main:def_lang, langs:all_langs};
 }
 
-multilang.generateButtons=function changeDoc(documentTextHeader,lang) {
-    var r=null;
+var imgUrl = 'https://github.com/codenautas/multilang/blob/master/img/';
+
+multilang.generateButtons=function generateButtons(documentTextHeader,lang) {
+    var obtainedLangs = multilang.obtainLangs(documentTextHeader);
+    var r = null;
     if(this.langs) {
         var ln=this.langs[lang];
         if(ln) {
             r='<!--multilang buttons -->\n';
-            r += 'idioma: !['+ln.name+'](https://github.com/codenautas/multilang/blob/master/img/lang-'+ln.abr+'.png)\n';
+            r += 'idioma: !['+ln.name+']('+imgUrl+'lang-'+ln.abr+'.png)\n';
             r += 'also available in:';
             for(var lother in ln.languages) {
                 var lname = ln.languages[lother];
                 if(ln.name == lname) { continue; }
-                r += '\n[!['+lname+'](https://github.com/codenautas/multilang/blob/master/img/lang-'+lother+'.png)](readme.md)';
+                r += '\n[!['+lname+']('+imgUrl+'lang-'+lother+'.png)]('+obtainedLangs.langs[lother].fileName+')';
             }
         }
     }
