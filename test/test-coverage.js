a4363c83a200f38e3dbc287cdaf3b6d3a2b2bc88
Diego's Drybeast <diegoefe@gmail.com>
2015-05-12 18:04:05 -0300

    coverage: varios tests de multilang.main y sus parameters

--- test/test-coverage.js
+++ test/test-coverage.js
@@ -9,12 +9,24 @@ var expectCalled = require('expect-called');
 var Promise = require('promise');
 var path = require('path');
  
-describe('multilang.main null input', function(){
+describe('multilang.main coverage', function(){
+    var obtainLangsControl;
+    var readFileControl;
+    var changeDocControl;
+    var writeFileControl;
+    beforeEach(function(){
+        obtainLangsControl=expectCalled.control(multilang,'obtainLangs',{returns:[{main:'mm', langs:{xx:{fileName:'xx.md'}}}]});
+        readFileControl =expectCalled.control(fs,'readFile',{returns:[Promise.resolve('content of INPUT')]});
+        changeDocControl=expectCalled.control(multilang,'changeDoc',{returns:['valid content']});
+        writeFileControl=expectCalled.control(fs,'writeFile',{returns:[Promise.resolve()]});
+    });
+    afterEach(function(){
+        readFileControl .stopControl();
+        changeDocControl.stopControl();
+        writeFileControl.stopControl();
+        obtainLangsControl.stopControl();
+    });
     it('do simple task, verbose',function(done){
-        var obtainLangsControl=expectCalled.control(multilang,'obtainLangs',{returns:[{main:'mm', langs:{xx:{fileName:'xx.md'}}}]});
-        var readFileControl =expectCalled.control(fs,'readFile',{returns:[Promise.resolve('content of INPUT')]});
-        var changeDocControl=expectCalled.control(multilang,'changeDoc',{returns:['valid content']});
-        var writeFileControl=expectCalled.control(fs,'writeFile',{returns:[Promise.resolve()]});
         var old_stdout = process.stdout.write;
         process.stdout.write = function() {};
         multilang.main({
@@ -33,11 +45,85 @@ describe('multilang.main null input', function(){
         }).catch(function(err){
             process.stdout.write = old_stdout;
             done(err);
-        }).then(function(){
-            readFileControl .stopControl();
-            changeDocControl.stopControl();
-            writeFileControl.stopControl();
-            obtainLangsControl.stopControl();
+        });
+    });
+    it('do another simple task, verbose',function(done){
+        var old_stdout = process.stdout.write;
+        process.stdout.write = function() {};
+        multilang.main({
+            input:'INPUT.md',
+            langs:null,
+            output:'OUTPUT.md',
+            directory:'aDirectory',
+            silent:false
+        }).then(function(exitCode){
+            process.stdout.write = old_stdout;
+            expect(readFileControl .calls).to.eql([['INPUT.md',{encoding: 'utf8'}]]);
+            expect(changeDocControl.calls).to.eql([['content of INPUT','xx']]);
+            expect(writeFileControl.calls).to.eql([['aDirectory'+path.sep+'OUTPUT.md','valid content']]);
+            expect(exitCode).to.eql(0);
+            done();
+        }).catch(function(err){
+            process.stdout.write = old_stdout;
+            done(err);
+        });
+    });
+    it('generating errors in parameters.langs',function(done){
+        var invalidLangs = ['mm'];
+        //console.log("obtainLangsControl", obtainLangsControl);
+        obtainLangsControl.remainReturns[0].langs = invalidLangs;
+        multilang.main({
+            input:'INPUT.md',
+            langs:invalidLangs,
+            output:'OUTPUT.md',
+            directory:'aDirectory',
+            silent:true
+        }).then(function(returnErr){
+            done(returnErr);
+        }).catch(function(err){
+            // console.log("err", err);
+            expect(writeFileControl.calls.length).to.eql(0);
+            expect(readFileControl .calls).to.eql([['INPUT.md',{encoding: 'utf8'}]]);
+            expect(changeDocControl.calls.length).to.eql(0);
+            done();
+        });
+    });
+    it('generating errors in parameters.langs & output',function(done){
+        var invalidLangs = ['mm', 'yy', 'pp'];
+        obtainLangsControl.remainReturns[0].langs = invalidLangs;
+        multilang.main({
+            input:'INPUT.md',
+            langs:invalidLangs,
+            output:'OUTPUT.md',
+            directory:'aDirectory',
+            silent:true
+        }).then(function(returnErr){
+            // console.log(returnErr);
+            done(returnErr);
+        }).catch(function(err){
+            //console.log("err", err);
+            expect(writeFileControl.calls.length).to.eql(0);
+            expect(readFileControl .calls).to.eql([['INPUT.md',{encoding: 'utf8'}]]);
+            expect(changeDocControl.calls.length).to.eql(0);
+            done();
+        });
+    });
+    it('generating errors in parameters.directory',function(done){
+        multilang.main({
+            input:'INPUT.md',
+            langs:['xx'],
+            output:'OUTPUT.md',
+            directory:null,
+            silent:true
+        }).then(function(returnErr){
+            // console.log(returnErr);
+            done(returnErr);
+        }).catch(function(err){
+            //console.log("err", err);
+            expect(writeFileControl.calls.length).to.eql(0);
+            expect(readFileControl .calls).to.eql([['INPUT.md',{encoding: 'utf8'}]]);
+            expect(changeDocControl.calls.length).to.eql(0);
+            done();
         });
     });
 });
